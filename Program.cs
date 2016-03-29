dbf625dec8803160311d76ecb195dc0b7d207ba6
etopiei <lj343@icloud.com>
2016-03-29 11:31:36 +1100

    Set-up classes and methods
    Set up the methods and classes to break down the problem and to clearly
    lay out steps.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Hangman
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman!");
+        }
+    }
+
+    class Game
+    {
+
+        public void ChooseWord()
+        {
+
+
+
+        }
+
+        public void ConvertWordToArray()
+        {
+
+
+
+        }
+
+        public void AcceptGuess()
+        {
+
+
+
+        }
+
+        public void CheckGuess()
+        {
+
+
+
+        }
+
+        public void OutputGuessResult()
+        {
+
+
+
+        }
+
+    }
+}
