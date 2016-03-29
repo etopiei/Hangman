fa24d86988132aa8e65bc6c2b3117665c00e1640
etopiei <lj343@icloud.com>
2016-03-29 22:08:59 +1100

    Changed how words are accessed
    Using file now.

--- WordList.cs
+++ WordList.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.IO;
 
 namespace Hangman
 {
     public class WordList
     {
-
-        string[] words = new string[] { "that", "have", "with", "this", "from", "they", "will", "would", "there", "their", "what", "about", "which", "when", "make", "like", "time", "just", "know", "take", "person", "into", "year", "your", "good", "some", "could", "them", "other", "than", "then", "look", "only", "come", "over", "think", "also", "back", "after", "work", "first", "well", "even", "want", "because", "these", "give", "most" , "aboard", "absorb", "afghan", "assign", "cement", "chorus", "cleave", "cornea", "crunch", "debris", "delved", "drawer", "duress", "easily", "embryo", "exotic", "eyelet", "fedora", "genius", "giggle", "gossip", "helmet", "heroic", "kidney", "loathe", "mallet", "mortar", "murmur", "nephew", "parody", "peruse", "ponder", "racial", "ransom", "rascal", "realty", "reason", "reckon", "relish", "rugged", "scythe", "sequel", "shovel", "simmer", "sourly", "stench", "talons", "tiring", "tissue", "virtue", "weasel", "widget", "wizard", "wombat" };
-
         public WordList()
         {
             
@@ -14,11 +12,10 @@ namespace Hangman
 
         public string ChooseRandomWord()
         {
-
-            Random rnd = new Random();
-            int arrayindex = rnd.Next (0, 47);
-            string chosenword = words[arrayindex];
-            return chosenword;
+            string path = @"Words.txt";
+            string[] lines = File.ReadAllLines(path);
+            Random rand = new Random();
+            return lines[rand.Next(lines.Length)];
 
         }
     }
