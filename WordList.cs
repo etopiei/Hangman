3a866933a665573c84a753018b6681e2c4019fb8
etopiei <lj343@icloud.com>
2016-03-29 20:28:43 +1100

    Added ~50 new words
    Added more words to the wordlist.

--- WordList.cs
+++ WordList.cs
@@ -5,7 +5,7 @@ namespace Hangman
     public class WordList
     {
 
-        string[] words = new string[] { "that", "have", "with", "this", "from", "they", "will", "would", "there", "their", "what", "about", "which", "when", "make", "like", "time", "just", "know", "take", "person", "into", "year", "your", "good", "some", "could", "them", "other", "than", "then", "look", "only", "come", "over", "think", "also", "back", "after", "work", "first", "well", "even", "want", "because", "these", "give", "most" };
+        string[] words = new string[] { "that", "have", "with", "this", "from", "they", "will", "would", "there", "their", "what", "about", "which", "when", "make", "like", "time", "just", "know", "take", "person", "into", "year", "your", "good", "some", "could", "them", "other", "than", "then", "look", "only", "come", "over", "think", "also", "back", "after", "work", "first", "well", "even", "want", "because", "these", "give", "most" , "aboard", "absorb", "afghan", "assign", "cement", "chorus", "cleave", "cornea", "crunch", "debris", "delved", "drawer", "duress", "easily", "embryo", "exotic", "eyelet", "fedora", "genius", "giggle", "gossip", "helmet", "heroic", "kidney", "loathe", "mallet", "mortar", "murmur", "nephew", "parody", "peruse", "ponder", "racial", "ransom", "rascal", "realty", "reason", "reckon", "relish", "rugged", "scythe", "sequel", "shovel", "simmer", "sourly", "stench", "talons", "tiring", "tissue", "virtue", "weasel", "widget", "wizard", "wombat" };
 
         public WordList()
         {
